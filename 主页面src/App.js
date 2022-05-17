05579dc3cef79ad76fa8e1771a3cd18450e4fb90
net开摆小分队 <36719447+Aated@users.noreply.github.com>
2022-05-17 11:31:54 +0800

    修改了App.js

--- 主页面src/App.js
+++ 主页面src/App.js
@@ -1,16 +1,14 @@
-import { HashRouter } from 'react-router-dom';
-import './App.css';
-import Home from './home/Home';
+import { HashRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./home/Home";
 
-
-
+//你好啊
 
 function App() {
   return (
     <HashRouter>
-  <Home/>
-  </HashRouter>
-   
+      <Home />
+    </HashRouter>
   );
 }
 
