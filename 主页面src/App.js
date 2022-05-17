58ff8bb8338796c310815f2f7d10de2f6145187e
net开摆小分队 <36719447+Aated@users.noreply.github.com>
2022-05-17 11:26:02 +0800

    上传了主页面src

---
+++ 主页面src/App.js
@@ -0,0 +1,17 @@
+import { HashRouter } from 'react-router-dom';
+import './App.css';
+import Home from './home/Home';
+
+
+
+
+function App() {
+  return (
+    <HashRouter>
+  <Home/>
+  </HashRouter>
+   
+  );
+}
+
+export default App;
---
+++ 主页面src/login/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <fieldset>导航</fieldset>
+      </div>
+    )
+  }
+}
